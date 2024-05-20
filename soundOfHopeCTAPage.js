79cb0ce71280489292089146de2615199306fa68
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2024-05-20 16:45:47 -0400

    Update soundOfHopeCTAPage.js

--- soundOfHopeCTAPage.js
+++ soundOfHopeCTAPage.js
@@ -1,1 +1,31 @@
+    window.addEventListener('scroll', ()=>{
+        const bg = document.querySelectorAll('.bg-highlight');
+        const rounds = document.querySelectorAll('.round');
 
+
+        bg.forEach(e => {
+            let t = e.getBoundingClientRect().top;
+
+            if(t <= window.innerHeight/1.5){
+                e.classList.add('bgSlide');
+            }
+        })
+
+        
+        rounds.forEach(e => {
+            let t = e.getBoundingClientRect().top;
+
+            if(t <= window.innerHeight/1.5){
+                e.classList.add('shown');
+            }
+        })
+    })
+
+    function ScrollToNext(value){
+        const row = document.querySelectorAll('.bcs-widget-row');
+        window.scrollTo({
+            top: row[value].getBoundingClientRect().top,
+            left: 0,
+            behavior: "smooth",
+        })
+    }

075cf0ee5462e90a51bea34966dda045370d32fc
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2024-05-24 14:07:26 -0400

    Cleaned js and js scroll to elment in page

--- soundOfHopeCTAPage.js
+++ soundOfHopeCTAPage.js
@@ -5,72 +5,110 @@
     var widgetsToAnimate = ['.inner-bcs-text.text-lg', '#w7 .card', '#contact p'];
 
     window.addEventListener('load', ()=>{
+        try{
+            fade.forEach(e => {
+                let t = e.getBoundingClientRect().top;
+                if(t <= window.innerHeight){
+                    e.classList.replace('fade-custom', 'fade-shown');
+                }
+            })
+        }catch(e){
 
-        fade.forEach(e => {
-            let t = e.getBoundingClientRect().top;
-            if(t <= window.innerHeight){
-                e.classList.replace('fade-custom', 'fade-shown');
-            }
-        })
+        }
     })
 
     window.addEventListener('scroll', ()=>{
-        var sy = window.scrollY;
-
-        widgetsToAnimate.forEach(w => {
-            let widget = document.querySelectorAll(w);
-            widget.forEach(el =>{
-                if(el.getAttribute('data-isset') != 'true'){
-                    el.setAttribute('data-isset', 'true');
-                    el.classList.add('fade-custom');
-                }
-            })
-        })
-
-        if(sy > (window.innerHeight + window.innerHeight/3)){
-            document.getElementById('t1').style="opacity: 0 !important; z-index: -999;";
-        }else {
-            document.getElementById('t1').style="";
-        }
-
-        document.querySelectorAll('.fade-custom').forEach(e => {
-            let t = e.getBoundingClientRect().top;
-
-            if(t <= window.innerHeight/1.5){
-                e.classList.replace('fade-custom', 'fade-shown');
-            }
-        })
-
+        animateStaticElements();
+        hideFirstElementAfterScroll();
+        fadeAnimation();
         if(document.getElementById('number').getAttribute('scrolled') != "true" && document.getElementById('number').getBoundingClientRect().top <= window.innerHeight/1.5){
             countNumbers();
-            document.getElementById('number').setAttribute('scrolled', "true");
         }
-
     })
+
+    function animateStaticElements(){
+        try{
+            widgetsToAnimate.forEach(w => {
+                let widget = document.querySelectorAll(w);
+                widget.forEach(el =>{
+                    if(el.getAttribute('data-isset') != 'true'){
+                        el.setAttribute('data-isset', 'true');
+                        el.classList.add('fade-custom');
+                    }
+                })
+            })
+        }catch(e){
+
+        }
+    }
+
+    function hideFirstElementAfterScroll(){
+        var sy = window.scrollY;
+        try {
+            if(sy > (window.innerHeight + window.innerHeight/3)){
+                document.getElementById('t1').style="opacity: 0 !important; z-index: -999;";
+            }else {
+                document.getElementById('t1').style="";
+            }
+        }catch(e){
+
+        }
+    }
+
+    function fadeAnimation(){
+        try{
+            document.querySelectorAll('.fade-custom').forEach(e => {
+                let t = e.getBoundingClientRect().top;
+    
+                if(t <= window.innerHeight/1.5){
+                    e.classList.replace('fade-custom', 'fade-shown');
+                }
+            })
+        }
+        catch(e){
+
+        }
+    }
 
     function countNumbers(){
         var starting = 375000;
         const incr = 125;
         var target = 400000;
         const time = 2.5;
-        setInterval(()=>{
-            if(starting < target){
-                starting += incr;
-                let tmp = starting.toString();
-                let a = tmp.substring(0, 3);
-                let b = tmp.substring(3, 6);
-                let str = a + ',' + b + '+';
-                document.getElementById('number').textContent = str;
-            }
-        }, time)
+        try{
+            setInterval(()=>{
+                if(starting < target){
+                    starting += incr;
+                    let tmp = starting.toString();
+                    let a = tmp.substring(0, 3);
+                    let b = tmp.substring(3, 6);
+                    let str = a + ',' + b + '+';
+                    document.getElementById('number').textContent = str;
+                }
+            }, time)
+            document.getElementById('number').setAttribute('scrolled', "true");
+        }catch(e){}
     }
 
-    const row = document.querySelectorAll('.bcs-widget-row');
-    scrollNextButtons.forEach(btn => btn.addEventListener('click', ()=>{
-        const headerHeight = document.getElementById('header-and-navigation-2024').clientHeight;
+    //scrollNextButtons.forEach(btn => btn.addEventListener('click', ()=>{
+      ///  try {
+         //   const headerHeight = document.getElementById('header-and-navigation-2024').clientHeight;
+           // let value = btn.getAttribute('data-target-element');
+            //let element = document.getElementById(value);
+           // window.scrollTo({
+           //     top: (element.getBoundingClientRect().top - headerHeight - 50),
+           //     left: 0,
+            //    behavior: "smooth"
+            //})
+        //}catch(e){
+         //   console.log(e);
+        //}
+    //}))
+
+    function jsScrollToPageElement(x){
         try {
-            let value = btn.getAttribute('data-target-element');
-            let element = document.getElementById(value);
+            const headerHeight = document.getElementById('header-and-navigation-2024').clientHeight;
+            let element = document.querySelector(x);
             window.scrollTo({
                 top: (element.getBoundingClientRect().top - headerHeight - 50),
                 left: 0,
@@ -79,78 +117,80 @@
         }catch(e){
             console.log(e);
         }
-    }))
+    }
 
 
     document.querySelector('.postalcode-input').addEventListener('input', (e)=>{
         try {
             volunteer(e.target.value);
         }catch(e){
-        }
 
+        }
     })
 
     function volunteer(value){
-        const go = document.getElementById('volunteer-go-btn');
-        const container = document.getElementById('volunteer-form');
-        const url = '/step-up/volunteer?postalCode=';
-        let postalCodeUrl = url + value;
-        go.setAttribute('href', postalCodeUrl);
-
-        if(value.toString().length == 5){
-            container.setAttribute('data-valid', "true");
-        }else {
-            container.setAttribute('data-valid', "false");
-        }
+        try{
+            const go = document.getElementById('volunteer-go-btn');
+            const container = document.getElementById('volunteer-form');
+            const url = '/step-up/volunteer?postalCode=';
+            let postalCodeUrl = url + value;
+            go.setAttribute('href', postalCodeUrl);
+    
+            if(value.toString().length == 5){
+                container.setAttribute('data-valid', "true");
+            }else {
+                container.setAttribute('data-valid', "false");
+            }
+        }catch(e){}
     }
 
 
     toMeasure.forEach(el =>{
-        let height = getLargestScrollCard();
-        el.style = 'max-height: ' + height + 'px; min-height: ' + height + 'px;';
-
-       // let w = document.querySelectorAll('.card')[1].clientWidth;
-       // let h = document.querySelectorAll('.card')[1].clientHeight;
-
-       // document.querySelector('.card.first').style = 'max-height: ' + h + 'px;' + 'min-height: ' + h + 'px;' + 'max-width: ' + w + 'px;' + 'mxin-width: ' + w + 'px;'
-
+        try{
+            let height = getLargestScrollCard();
+            el.style = 'max-height: ' + height + 'px; min-height: ' + height + 'px;';
+        }catch(e){}
     })
 
     function getLargestScrollCard(){
-        var init = 0;
-        toMeasure.forEach(m => {
-            if(m.clientHeight > init){
-                init = m.clientHeight;
-            }
-        })
-        return init;
+        try{
+            var init = 0;
+            toMeasure.forEach(m => {
+                if(m.clientHeight > init){
+                    init = m.clientHeight;
+                }
+            })
+            return init;
+        }catch(e){
+
+        }
     }
 
     const scrolls = document.querySelectorAll('.scroll-arrow');
     scrolls.forEach(sc => sc.addEventListener('click', ()=>{
-
-        const container = document.querySelector('.cards-container');
-        const superW = document.querySelector('.cv').clientWidth;
-        
-        switch (sc.getAttribute('data-direction')){
-            case "right":
-                let r = '+=' + superW;
-                  $(container).animate({
-                    scrollLeft: r
-                  }, 300, 'swing');
-                break;
-            case "left":
-                    let l = '-=' + superW;
-                  $(container).animate({
-                    scrollLeft: l
-                  }, 300, 'swing');
-                break;
-            case "right-super":
-                let t = '+=' + superW;
-                  $(container).animate({
-                    scrollLeft: t
-                  }, 300, 'swing');
-                break;
-        }
-
+        try{
+            const container = document.querySelector('.cards-container');
+            const superW = document.querySelector('.cv').clientWidth;
+            
+            switch (sc.getAttribute('data-direction')){
+                case "right":
+                    let r = '+=' + superW;
+                      $(container).animate({
+                        scrollLeft: r
+                      }, 300, 'swing');
+                    break;
+                case "left":
+                        let l = '-=' + superW;
+                      $(container).animate({
+                        scrollLeft: l
+                      }, 300, 'swing');
+                    break;
+                case "right-super":
+                    let t = '+=' + superW;
+                      $(container).animate({
+                        scrollLeft: t
+                      }, 300, 'swing');
+                    break;
+            }
+        }catch(e){}
     }))

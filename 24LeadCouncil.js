f7a098885e94bdde614d6a4d0f6f56bbdebcd8c5
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2024-07-11 15:07:04 -0400

    Update 24LeadCouncil.js

--- 24LeadCouncil.js
+++ 24LeadCouncil.js
@@ -1,17 +1,21 @@
-    function dayScheduleSlider(daySchedule, markerPosition, setActive){
-        const toggleOptions = ['.toggle.start-here', '.toggle.thu', '.toggle.fri', '.toggle.sat'];
-        const dayOptions = ['.day-schedule.start-here', '.day-schedule.thu', '.day-schedule.fri', '.day-schedule.sat'];
+function dayScheduleSlider(daySchedule, markerPosition, setActive){
+    const toggleOptions = ['.toggle.start-here', '.toggle.thu', '.toggle.fri', '.toggle.sat'];
+    const dayOptions = ['.day-schedule.start-here', '.day-schedule.thu', '.day-schedule.fri', '.day-schedule.sat'];
 
-        const marker = document.querySelector('.schedule-toggles');
-        var day = document.querySelector(daySchedule);
+    const marker = document.querySelector('.schedule-toggles');
+    var day = document.querySelector(daySchedule);
 
-        toggleOptions.forEach(o => {
-            if(o != setActive){
-                document.querySelector(o).setAttribute('data-active', 'false')
-            }else {
-                document.querySelector(o).setAttribute('data-active', 'true')
-            }
-        })
-        
-        marker.setAttribute('data-top', markerPosition);
-    }
+    toggleOptions.forEach(o => {
+        if(o != setActive){
+            document.querySelector(o).setAttribute('data-active', 'false')
+        }else {
+            document.querySelector(o).setAttribute('data-active', 'true')
+        }
+    })
+    
+    marker.setAttribute('data-top', markerPosition);
+}
+
+document.querySelectorAll('.toggle').forEach(t => t.addEventListener('click',()=>{
+    dayScheduleSlider(t.getAttribute('data-day'), t.getAttribute('data-top'), t.getAttribute('set-active'));
+}))

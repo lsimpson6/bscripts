8a92e199333de425df0d73315d8ee60fc22bdca5
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2024-07-11 15:10:34 -0400

    Update 24LeadCouncil.js

--- 24LeadCouncil.js
+++ 24LeadCouncil.js
@@ -1,4 +1,4 @@
-function dayScheduleSlider(daySchedule, markerPosition, setActive){
+function dayScheduleSlider(t, daySchedule, markerPosition, setActive){
     const toggleOptions = ['.toggle.start-here', '.toggle.thu', '.toggle.fri', '.toggle.sat'];
     const dayOptions = ['.day-schedule.start-here', '.day-schedule.thu', '.day-schedule.fri', '.day-schedule.sat'];
 
@@ -12,10 +12,12 @@ function dayScheduleSlider(daySchedule, markerPosition, setActive){
             document.querySelector(o).setAttribute('data-active', 'true')
         }
     })
-    
+    console.log(t);
     marker.setAttribute('data-top', markerPosition);
 }
 
 document.querySelectorAll('.toggle').forEach(t => t.addEventListener('click',()=>{
-    dayScheduleSlider(t.getAttribute('data-day'), t.getAttribute('data-top'), t.getAttribute('set-active'));
+    console.log(t);
+    dayScheduleSlider(t, t.getAttribute('data-day'), t.getAttribute('data-top'), t.getAttribute('set-active'));
+    console.log(t.getAttribute('data-day'), t.getAttribute('data-top'), t.getAttribute('set-active'));
 }))

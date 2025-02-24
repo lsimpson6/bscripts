0a952f1d8dbb2b90d6c0ddb6c15a0b68203e61a3
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2025-02-24 15:13:15 -0500

    Update stepin-async.js

--- stepin-async.js
+++ stepin-async.js
@@ -4,15 +4,14 @@ window.onload = function() {
     var paramsT = window.location.search;
     let widthT = window.innerWidth;
 
-    if(widthT != initWindowWidthT){
-        if(widthT < 801){
-            if(currentWindowPathT != '/campaign/step-in'){
-                window.location.href = 'https://bethany.org/campaign/step-in' + paramsT;
-            }        
-            }else{
-            if(currentWindowPathT != '/campaigns/step-in/dev'){
-                window.location.href = 'https://bethany.org/campaigns/step-in' + paramsT;
-            }
+    if(widthT < 801){
+        if(currentWindowPathT != '/campaign/step-in'){
+            window.location.href = 'https://bethany.org/campaign/step-in' + paramsT;
+        }        
+        }else{
+        if(currentWindowPathT != '/campaigns/step-in/dev'){
+            window.location.href = 'https://bethany.org/campaigns/step-in' + paramsT;
         }
     }
+
 };
